b52362436e577aca00ca63908f80133b1dbc97e5
Anthony Siletti <anthony.siletti@gmail.com>
2020-08-05 20:51:40 -0400

    working on edit

--- client/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
+++ client/src/app/components/recipes/edit-recipe/edit-recipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from 'src/app/models/Recipe';
 import { RecipesService } from 'src/app/services/recipes.service';
 import { ActivatedRoute } from '@angular/router';
+import { RecipeStep } from 'src/app/models/RecipeStep';
 
 @Component({
   selector: 'app-edit-recipe',
@@ -9,8 +9,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./edit-recipe.component.scss'],
 })
 export class EditRecipeComponent implements OnInit {
-  recipe: Recipe;
   id: string;
+  ingredients: string;
+  recipeTitle: string;
+  recipeDescription: string;
+  recipeImage: string;
+  recipeSteps: RecipeStep[];
+  counter = 0;
+
   constructor(
     private recipeService: RecipesService,
     private router: ActivatedRoute
@@ -21,11 +27,24 @@ export class EditRecipeComponent implements OnInit {
     this.recipeService.getRecipeById(this.id).subscribe(
       (data) => {
         console.log(data);
-        this.recipe = data;
+        this.ingredients = data.ingredients;
+        this.recipeTitle = data.recipeTitle;
+        this.recipeDescription = data.recipeDescription;
+        this.recipeImage = data.recipeImage;
+        this.recipeSteps = data.recipeSteps;
       },
       (error) => {
         console.log(error);
       }
     );
   }
+
+  next(): void {
+    if (this.counter < this.recipeSteps.length) this.counter++;
+  }
+  prev(): void {
+    if (this.counter >= 0) this.counter--;
+  }
+
+  submitEdit(): void {}
 }
